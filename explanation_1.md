b69ae1c0d15d9f9e342f142dd50761be33be93ea
Fan Yuan <yfan941101@gmail.com>
2022-03-23 03:55:08 -0400

    Complete and upload problem 1 solution and explanation

--- explanation_1.md
+++ explanation_1.md
@@ -1,20 +1,14 @@
 # Problem 1: LRU Cache
 
 ## Reason
-In order to make all operations to O(1) time, here decide to use dictionary. We also need to track the first and last element, to achieve this we use deque in python collections module so that we can reach O(1) when moving the element. In other word, we keep the order of elements in deque, and retrieve the key in order and get the value of that key from dictionary.
+In order to make all operations to O(1) time, here decide to use dictionary to store the (key, value) pairs. We also need to track the order of elements being inserted so that we can remove the least recently used when hitting the max_capacity, to achieve this we use deque in python collections module to reach O(1) when moving the oldest element. In other word, we track the order of elements being inserted in deque, and retrieve the key in order and get the value of that key from dictionary.
 
 ## Efficiency
 
 - Time complexity
-  - get: O(1)
-  - set: O(1)
-
-
-accessing and setting the values through a dictionary takes constant time.
-accessing the first and last element is constant time as well using the head and tail of the deque.
-rearranging the elements when we want to move something to the top or drop an element is also constant time because we are using a doubly linked list
-
+  - get: O(1), traverse and get values through dictionary takes constant time
+  - set: O(1), access the value, move elements with deque both take constant time
 
 - Space complexity
-  - set: O(n) where n is the capacity since we create that big dictionary to store the data
   - get: O(1) use constant memory when getting a value
+  - set: O(n) where n is the capacity since we create that big dictionary to store the data
--- problem_1.py
+++ problem_1.py
@@ -1,42 +1,72 @@
 '''
-design a data structure known as a Least Recently Used (LRU) cache
-All operations must take O(1) time.
-    In case of a cache hit, your get() operation should return the appropriate value.
-    In case of a cache miss, your get() should return -1.
-While putting an element in the cache, put() / set() operation will insert the element if the key is not present.
-If the cache is full, removes the least recently used entry first and then insert the element.
+Design a data structure known as a Least Recently Used (LRU) cache.
+
+- All operations must take O(1) time.
+- When cache hit, get() operation returns the appropriate value.
+- When cache miss, get() returns -1.
+- While putting an element in the cache, put() / set() operation must insert the
+element. If the cache is full, removes the least recently used entry first and
+then insert the new element.
+For the current problem, consider the size of cache = 5.
 '''
 
 from collections import deque
 
 class LRU_Cache(object):
 
-    def __init__(self, capacity):
+    def __init__(self, capacity=None):
         # Initialize class variables
-        self.max_capacity = capacity
-        self.cache_data = {}
-        self.cache_order = deque()
+        self.max_capacity = capacity # set max capacity
+        self.cache_data = {} # store key,value pairs
+        self.cache_order = deque() # track the order of key,value insert
 
     def get(self, key):
-        # Retrieve item from provided key. Return -1 if nonexistent.
+        """
+        Retrieve item from provided key. Return -1 if nonexistent.
+        Input:
+            self, key value
+        Output:
+            value: value of input key (-1 if input key not exists)
+        """
         if key is None:
             return -1
-        return self.cache_data.get(key, -1)
+        return self.cache_data.get(key, -1) # return -1 if key not exists
 
     def set(self, key, value):
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+        """
+        Set the value if the key is not present in the cache.
+        If the cache is at capacity remove the oldest item.
+        Input:
+            self, key, value
+        """
+        # check none max_capacity
+        if self.max_capacity is None:
+            print("Error: Cache capacity couldn't be None")
+            return
+
         # check empty capacity
-        if self.max_capacity == 0:
+        if self.max_capacity <= 0:
             print("Error: Cache capacity need to be greater than 0")
             return
 
-        # print('cache size ', len(self.cache_order))
+        # check the key
+        if key is None:
+            print("Error: Key couldn't be None")
+            return
+
+        # check max_capacity and insert values
         if len(self.cache_order) == self.max_capacity:
-            rm_elem = self.cache_data[self.cache_order.popleft()]
-            # print(rm_elem,'deleted')
-            del self.cache_data[rm_elem]
-        self.cache_order.append(key)
+            print("\nHit the max capacity")
+
+            # remove the least recently used entry
+            rm_lru = self.cache_data[self.cache_order.popleft()] # pop the oldest
+            print('(', rm_lru, ', ', self.cache_data[rm_lru],') deleted\n')
+            del self.cache_data[rm_lru]
+
+        self.cache_order.append(key) # track the order of key,value insert
         self.cache_data[key] = value
+        print('(', key, ', ', value, ') inserted')
+
 
 print('----------Test1----------')
 our_cache = LRU_Cache(5)
@@ -47,17 +77,17 @@ our_cache.set(3, 3)
 our_cache.set(4, 4)
 
 
-print(our_cache.get(1))       # returns 1
-print(our_cache.get(2))       # returns 2
-print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+print('Get value of 1: ', our_cache.get(1))       # returns 1
+print('Get value of 2: ', our_cache.get(2))       # returns 2
+print('Get value of 9: ', our_cache.get(9))      # returns -1 because 9 is not present in the cache
 
 our_cache.set(5, 5)
-our_cache.set(6, 6)
-our_cache.set(7, 7)
+our_cache.set(6, 6)     # Hit the max capacity. 1 ,  1  deleted
+our_cache.set(7, 7)     # Hit the max capacity. 2 ,  2  deleted
 
-print(our_cache.get(1))      # returns -1 because the cache reached it's capacity and 1 was the least recently used entry
-print(our_cache.get(2))      # returns -1 because the cache reached it's capacity and 2 was the least recently used entry
-
+print('Get value of 1: ', our_cache.get(1))      # returns -1 because (1,1) has been removed when the cache reached it's capacity
+print('Get value of 2: ', our_cache.get(2))      # returns -1 because (2,2) has been removed when the cache reached it's capacity
+print('\n')
 
 print('----------Test2----------')
 our_cache = LRU_Cache(3)
@@ -66,20 +96,29 @@ our_cache.set(1, 1)
 our_cache.set(2, 2)
 our_cache.set(3, 3)
 
-our_cache.set(1, 4) # because the cache reached it's capacity and 1 will be removes since it was the least recently used entry
-our_cache.set(2, 5) # because the cache reached it's capacity and 2 will be removes since it was the least recently used entry
-our_cache.set(3, 6) # because the cache reached it's capacity and 3 will be removes since it was the least recently used entry
+our_cache.set(1, 4) # Hit the max capacity. 1 ,  1  deleted
+our_cache.set(2, 5) # Hit the max capacity. 2 ,  2  deleted
+our_cache.set(3, 6) # Hit the max capacity. 3 ,  3  deleted
 
-print(our_cache.get(1))       # returns 4
-print(our_cache.get(2))       # returns 5
-print(our_cache.get(3))      # returns 6
+print('Get value of 1: ', our_cache.get(1))     # returns 4
+print('Get value of 2: ', our_cache.get(2))     # returns 5
+print('Get value of 3: ', our_cache.get(3))     # returns 6
 
-print(our_cache.get(6))      # returns -1 because 6 is not present in the cache
-
+print('Get value of 6: ', our_cache.get(6))      # returns -1 because 6 is not present in the cache
+print('\n')
 
 print('----------Test3----------')
-our_cache = LRU_Cache(0) # pop error when setting value on 0 capcacity
+# test None max_capacity
+our_cache = LRU_Cache()     # Error: Cache capacity couldn't be None
+our_cache.set(1, 1)
 
-our_cache.set(1, 1)    # Error: Cache capacity need to be greater than 0
+# test 0 max_capacity
+our_cache = LRU_Cache(0)    # Error: Cache capacity need to be greater than 0
+our_cache.set(1, 1)
 
-print(our_cache.get(1))     # returns -1 because 1 is not present in the cache
+# test none key
+our_cache = LRU_Cache(2)
+our_cache.set(None, 2)      # Error: Key couldn't be None
+
+print(our_cache.get(1))     # returns -1
+print('\n')
